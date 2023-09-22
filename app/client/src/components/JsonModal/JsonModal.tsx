35bfa7fe3c694269963cb54a0c41537b21bfa8a9
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-09-22 10:14:44 +0100

    Adding in modal display

--- app/client/src/components/JsonModal/JsonModal.tsx
+++ app/client/src/components/JsonModal/JsonModal.tsx
@@ -1,6 +1,6 @@
-import { Badge, BoxProps, Button, Code, Group, Modal } from '@mantine/core';
-import { Prism } from '@mantine/prism';
+import { Badge, Button, Code, Group, Modal } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { Prism } from '@mantine/prism';
 import { IconEye } from '@tabler/icons';
 
 interface JsonModalProps {
@@ -10,45 +10,31 @@ interface JsonModalProps {
 
 export const JsonModal = ({ json, apiEndpoint }: JsonModalProps) => {
   const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <>
-      {json ? (
-        <>
-          <Modal
-            opened={opened}
-            onClose={close}
-            size="xl"
-            title={
-              <Group spacing={4} align="center">
-                <Badge variant="filled" color="green" radius="xs">
-                  GET
-                </Badge>
-                <Code sx={{ backgroundColor: 'unset' }}>{apiEndpoint}</Code>
-              </Group>
-            }
-          >
-            <Prism
-              language="json"
-              styles={{ line: { width: 'unset' } }}
-              radius={0}
-            >
-              {JSON.stringify(json, null, 4)}
-            </Prism>
-          </Modal>
-          <Group position="right" mb={4}>
-            <Button
-              size="xs"
-              compact
-              leftIcon={<IconEye size={16} />}
-              variant="subtle"
-              color="gray"
-              onClick={open}
-            >
-              View JSON
-            </Button>
+      <Modal
+        opened={opened}
+        onClose={close}
+        size="xl"
+        title={
+          <Group spacing={4} align="center">
+            <Badge variant="filled" color="green" radius="xs">
+              GET
+            </Badge>
+            <Code sx={{ backgroundColor: 'unset' }}>{apiEndpoint}</Code>
           </Group>
-        </>
-      ) : null}
+        }
+      >
+        <Prism language="json" styles={{ line: { width: 'unset' } }} radius={0}>
+          {JSON.stringify(json, null, 4)}
+        </Prism>
+      </Modal>
+      <Group position="center" >
+        <Button size="xs" compact variant="subtle" color="gray" onClick={open}>
+          <IconEye size={16} />
+        </Button>
+      </Group>
     </>
   );
 };
--- app/client/src/features/onlineGoodsAndServices/PaymentTransactionTable.tsx
+++ app/client/src/features/onlineGoodsAndServices/PaymentTransactionTable.tsx
@@ -1,6 +1,6 @@
 import { Text } from '@mantine/core';
 import { useQueries } from '@tanstack/react-query';
-import { Panel, TableWithJsonDisplay } from 'components';
+import { JsonModal, Panel, TableWithJsonDisplay } from 'components';
 import { useGetPayment } from './hooks/useGetPayment';
 import { paymentAuthorizeResponseListMock } from 'mocks/paymentAuthorizeResponseList.mock';
 import { paymentResponse } from 'generated-api-models';
@@ -25,7 +25,11 @@ export const PaymentTransactionTable = ({
   const isLoading = transactions.some((query) => query.isLoading);
 
   const createRow = (rowData: paymentResponse) => (
-    <tr key={rowData.transactionId} onClick={() => console.log('hello')}>
+    <tr key={rowData.transactionId}>
+      <JsonModal
+        json={rowData}
+        apiEndpoint={`/payments/${rowData.transactionId}`}
+      />
       <td>{rowData.transactionId}</td>
       <td>{rowData.requestId}</td>
       <td>{rowData.transactionDate}</td>
@@ -44,9 +48,12 @@ export const PaymentTransactionTable = ({
 
   const ths = (
     <tr>
+      <th></th>
       <th>Transaction ID</th>
       <th>Request ID</th>
+      <th>Transaction Date</th>
       <th>Transaction State</th>
+
       <th>Amount</th>
     </tr>
   );
@@ -58,11 +65,8 @@ export const PaymentTransactionTable = ({
       apiEndpoint="/payments/{id}"
     >
       <Text>You can use this call to return a specific transaction</Text>
-      <TableWithJsonDisplay
-        ths={ths}
-        apiEndpoint="/payments/{id}"
-        rows={rows}
-      />
+
+      <TableWithJsonDisplay ths={ths} rows={rows} />
     </Panel>
   );
 };
