9223d5e2075c67f4b6c3d687ce3a92143869429b
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-09-26 13:51:33 +0100

    Fix mobile

--- app/client/src/components/TableWithJsonDisplay/TableWithJsonDisplay.tsx
+++ app/client/src/components/TableWithJsonDisplay/TableWithJsonDisplay.tsx
@@ -14,21 +14,23 @@ export const TableWithJsonDisplay = ({
   isLoading,
 }: TableWithJsonDisplayProps) => {
   return (
-    <Table striped withColumnBorders withBorder >
-      <thead>{ths}</thead>
-      <tbody>
-        {isLoading
-          ? Array(2)
-              .fill(0)
-              .map((_, index) => (
-                <tr key={index}>
-                  <td colSpan={100}>
-                    <Skeleton h={22} />
-                  </td>
-                </tr>
-              ))
-          : rows}
-      </tbody>
-    </Table>
+    <ScrollArea offsetScrollbars type="auto">
+      <Table striped withColumnBorders withBorder>
+        <thead>{ths}</thead>
+        <tbody>
+          {isLoading
+            ? Array(2)
+                .fill(0)
+                .map((_, index) => (
+                  <tr key={index}>
+                    <td colSpan={100}>
+                      <Skeleton h={22} />
+                    </td>
+                  </tr>
+                ))
+            : rows}
+        </tbody>
+      </Table>
+    </ScrollArea>
   );
 };
